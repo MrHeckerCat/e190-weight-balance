b899a598b338906ce6ec2583bc84d2b5ca3be54c
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2025-04-22 20:57:47 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'e190-wb-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/Rajdhani-Regular.ttf',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
+});
